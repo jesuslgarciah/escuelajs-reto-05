021b2132568558606ba90c823eeb6a755a030328
Jesus Garcia <jelegaho@gmail.com>
2020-09-05 09:08:10 -0500

    challenge finished

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -2,15 +2,15 @@
 
 Solución al reto 05 de Escuela de JavaScript
 
-Nombre:
-Usuario Platzi:
+Nombre: Jesus Leonardo Garcia Hoyos
+Usuario Platzi: jesuslgarciah
 
 ## Ciudad
 - [ ] Ciudad de México
-- [ ] Bogotá
+- [x] Bogotá
 
 ## Reto:
-  - [ ] Primer problema
-  - [ ] Segundo problema
-  - [ ] Tercer problema
-  - [ ] Cuarto Problema (Opcional)
+  - [x] Primer problema
+  - [x] Segundo problema
+  - [x] Tercer problema
+  - [x] Cuarto Problema (Opcional)
--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
+const final = document.getElementById('final')
 const API = 'https://rickandmortyapi.com/api/character/';
 
+document.addEventListener("visibilitychange", async () => {
+  if(document.visibilityState !== "visible"){
+    $app.innerHTML = ""
+    localStorage.clear()
+    final.style.display = "none"
+  }else{
+    intersectionObserver.observe($observe);
+  }
+})
+
 const getData = api => {
+  if (localStorage.getItem('next_fetch') !== null){
+    api = localStorage.getItem('next_fetch')
+  }
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      const nextPage = response.info.next;
       const characters = response.results;
       let output = characters.map(character => {
         return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
+          <article class="Card">
+            <img src="${character.image}" />
+            <h2>${character.name}<span>${character.species}</span></h2>
+          </article>`
       }).join('');
       let newItem = document.createElement('section');
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      localStorage.setItem('next_fetch', nextPage)
+      if(nextPage === null){
+        intersectionObserver.disconnect()
+        final.style.display = "block"
+      }
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  try{
+    await getData(API);
+  }catch(err){
+    console.error(err)
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
